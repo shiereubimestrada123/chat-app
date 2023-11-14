e208f522f29322ffa579c4f650538df22045249f
shiereubimestrada123 <shiereubim.estrada@gmail.com>
2023-11-14 19:32:14 +0800

    Initial setup client

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from '@/pages/login/Login';
+import { Register } from '@/pages/register/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
